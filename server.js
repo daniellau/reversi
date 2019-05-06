971e8840c5717fa662ab4f1b49e69f73ae4fff08
Daniel Lau <daniel.lau@intusurg.com>
2019-05-06 14:06:23 -0700

    updating gitignore

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var app = express();
+const http = require('http');
 
-app.get('/', function(req, res) {
-    res.sendfile('index.html');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
